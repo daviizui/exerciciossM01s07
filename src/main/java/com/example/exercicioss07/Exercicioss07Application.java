84f4fd89ec1b06ddc6a41b322e59ff6912a479e4
Davi Izui <davi.izui@gmail.com>
2025-04-08 12:30:05 -0300

    first commit

---
+++ src/main/java/com/example/exercicioss07/Exercicioss07Application.java
@@ -0,0 +1,13 @@
+package com.example.exercicioss07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicioss07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicioss07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercicioss07/Exercicioss07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicioss07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicioss07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
